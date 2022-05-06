de97f5b89d9208ed85472f300a528a65926472e3
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2022-05-06 17:44:10 +0530

    Update colors.properties.sh

--- colors.properties.sh
+++ colors.properties.sh
@@ -1,5 +1,31 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+clear='\033[0m'
+Black='\033[0;30m'
+Red='\033[0;31m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Blue='\033[0;34m'
+Purple='\033[0;35m'
+Cyan='\033[0;36m'
+White='\033[0;37m'
+
+
+
+clear
+echo -e "
+                
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+    |M|e|t|a|s|p|l|o|i|t| |i|n| |T|e|r|m|u|x|
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+            +-+-+ +-+-+ +-+-+-+-+
+            |b|y| |M|R| |J|I|N|N|
+            +-+-+ +-+-+ +-+-+-+-+
+        ${Green}[ ${Yellow}@MrJinnTelegram${clear} ${Green}]${clear}
+"
+
+echo "Please wait..."
+
 cd $HOME
 cd .termux
 cp colors.properties ../;rm colors.properties
